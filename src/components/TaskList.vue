0489bdd9467dd551dedd9c380e3771b29bc13117
Alexander Matte <alex.matte71@gmail.com>
2024-04-01 20:57:34 +0200

    Added styling to icons and text

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -16,6 +16,9 @@ const toggleCompleted = (task) => {
   task.completed = !task.completed
 }
 </script>
+
+
+
 <template>
   <div id="task-list">
     <div
@@ -27,6 +30,7 @@ const toggleCompleted = (task) => {
         <div v-if="task.completed" @click="toggleCompleted(task)">
           <svg style="width: 1.7em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path
+              fill="hsla(160, 100%, 37%, 1)"
               d="M256 48a208 208 0 1 1 0 416 208 208 0 1 1 0-416zm0 464A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-111 111-47-47c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9l64 64c9.4 9.4 24.6 9.4 33.9 0L369 209z"
             />
           </svg>
@@ -34,22 +38,31 @@ const toggleCompleted = (task) => {
         <div v-else @click="toggleCompleted(task)">
           <svg style="width: 1.7em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path
+              fill="#F2F3F4"
               d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z"
             />
           </svg>
         </div>
       </div>
       <div
-        class="task-title mx-3"
+        class="task-title mx-3 green"
         :class="{ completed: task.completed }"
         @click="toggleCompleted(task)"
       >
         {{ task.title }}
       </div>
       <div class="ms-auto">
-        <button type="button" class="btn btn-primary btn-sm mr-2">Edit</button>
-        <button type="button" class="btn btn-danger btn-sm" @click="deleteTask(index)">
-          Delete
+        <button type="button" class="btn btn-sm">
+          <router-link :to="{ name: 'task', params: { id: task.id } }">Edit</router-link>
+        </button>
+
+        <button type="button" class="btn btn-sm" @click="deleteTask(index)">
+          <svg style="width: 1.7em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+            <path
+              fill="#FF0000"
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            />
+          </svg>
         </button>
       </div>
     </div>
@@ -60,5 +73,9 @@ const toggleCompleted = (task) => {
 <style scoped>
 .completed {
   text-decoration: line-through;
+}
+
+.task-item:hover {
+  cursor: pointer;
 }
 </style>
