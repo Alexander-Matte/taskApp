03b87af981a5b6ad013eefe4723ab623a211f33b
Alexander Matte <alex.matte71@gmail.com>
2024-05-04 16:56:17 +0200

    Created computed getter to return editing state. Changed old computed to method. Added localstorage state

--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia'
 
 export const useTaskStore = defineStore('tasks', () => {
   const tasks = ref([])
+  const descriptionEditingStatus = ref(false)
 
   async function fetchData() {
     try {
@@ -17,25 +18,62 @@ export const useTaskStore = defineStore('tasks', () => {
     }
   }
 
-  const getTaskById = computed(() => {
-    return (taskId) => tasks.value.find((task) => task.id === taskId)
+  const getDescriptionEditingStatus = computed(() => {
+    return descriptionEditingStatus.value
   })
+
+  function getTaskById(taskId) {
+    return tasks.value.find((task) => task.id === taskId)
+  }
 
   function addTask(task) {
     tasks.value.push(task)
+    saveState()
+  }
+
+  function updateTaskDescription(taskId, newDescription) {
+    const taskIndex = tasks.value.findIndex((task) => task.id === taskId)
+    if (taskIndex !== -1) {
+      const updatedTask = { ...tasks.value[taskIndex], description: newDescription }
+      tasks.value.splice(taskIndex, 1, updatedTask)
+      saveState()
+    }
+  }
+
+  function setEditingStatus(status) {
+    descriptionEditingStatus.value = status
   }
 
   function deleteTask(task) {
     const index = tasks.value.findIndex((t) => t.id === task.id)
     if (index !== -1) {
       tasks.value.splice(index, 1)
+      saveState()
     }
   }
+
+  function saveState() {
+    localStorage.setItem('taskStore', JSON.stringify(tasks.value))
+  }
+
+  function restoreState() {
+    const storedTasks = localStorage.getItem('taskStore')
+    if (storedTasks) {
+      tasks.value = JSON.parse(storedTasks)
+    }
+  }
+
+  // Call restoreState on application initialization
+  fetchData()
+  restoreState()
 
   return {
     addTask,
     deleteTask,
     fetchData,
+    getDescriptionEditingStatus,
+    updateTaskDescription,
+    setEditingStatus,
     getTaskById,
     tasks
   }
