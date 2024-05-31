f10cbd880fe191cc6c8144fb61465f62a6442c2b
Alexander Matte <alex.matte71@gmail.com>
2024-05-31 17:57:05 +0200

    Moved files into nuxt application

---
+++ myApp/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@pinia/nuxt', "usebootstrap"],
+  usebootstrap: {
+    bootstrap: {
+      prefix: ``
+    },
+    html: {
+      prefix: `B`
+    },
+  },
+  css: [
+    "bootstrap/scss/bootstrap.scss",
+    '~/assets/css/main.css'
+  ],
+  pages: true,
+
+  
+})
---
+++ myApp/pages/NotFound.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center">404 Not Found</h1>
+  </div>
+</template>
