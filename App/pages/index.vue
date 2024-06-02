549445d8b2ba7b785c42fff186fa556b4e1fb3de
Alexander Matte <alex.matte71@gmail.com>
2024-06-02 20:35:05 +0200

    Created grid layout

--- App/pages/index.vue
+++ App/pages/index.vue
@@ -35,30 +35,52 @@ function addTask() {
 </script>
 
 <template>
-  <div class="container mt-5">
-    <h1 class="text-center">Task List</h1>
-    <div class="row mb-3">
-      <div class="col">
-        <form @submit.prevent="addTask">
-          <div class="input-group">
-            <input
-              v-model="newTaskText"
-              type="text"
-              class="form-control"
-              placeholder="Add new task"
-              aria-label="Add new task"
-              aria-describedby="addTaskBtn"
-            />
-            <div class="input-group-append">
-              <button class="btn btn-primary" type="submit" id="addTaskBtn">
-                Add Task
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
+  <div class="container" id="home-container">
+    <div class="sidebar-header">
+      <div>Categories:</div>
     </div>
+    <div class="sidebar-content">
+      <ul>
+        <li>Test1</li>
+        <li>Test2</li>
+        <li>Test3</li>
+        <li>Test4</li>
+      </ul>
+    </div>
+    <div class="body-header">
+      <h1 class="text-center">Task List</h1>
+    </div>
+    <div class="body-content">
+      <div class="row mb-3">
+        <div class="col">
+          <form @submit.prevent="addTask">
+            <div class="input-group">
+              <input
+                v-model="newTaskText"
+                type="text"
+                class="form-control"
+                placeholder="Add new task"
+                aria-label="Add new task"
+                aria-describedby="addTaskBtn"
+              />
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="submit" id="addTaskBtn">
+                  Add Task
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
 
-    <TaskList />
+      <TaskList />
+    </div>
+    <div class="footer">
+      <ul class="d-flex justify-content-between">
+        <li>Footer Item 1</li>
+        <li>Footer Item 2</li>
+        <li>Footer Item 3</li>
+      </ul>
+    </div>
   </div>
 </template>
