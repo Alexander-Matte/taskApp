8fbff552ab12dbc2feb92400b006bb33d2505d8d
Alexander Matte <alex.matte71@gmail.com>
2024-06-03 21:25:18 +0200

    Changed svgs for bootstrap icons

--- App/pages/task/[id].vue
+++ App/pages/task/[id].vue
@@ -3,31 +3,36 @@ import Toastify from "toastify-js";
 import "toastify-js/src/toastify.css";
 import { ref, computed, watchEffect, onBeforeUnmount } from "vue";
 import dayjs from "dayjs";
+
+/* Start Task Store and fetch task  */
 const { id } = useRoute().params;
-
-const titleInput = ref(null);
-watchEffect(() => {
-  if (titleInput.value) {
-    titleInput.value.focus();
-  }
-});
-
 const store = await useTaskStore();
 await store.fetchTasks();
 const task = computed(() => {
   return store.getTaskById(id);
 });
 
+/* Variable initializations  */
 const taskTitle = ref(task.value.title);
 const titleEditing = ref(false);
+const titleInput = ref(null);
+const description = ref(task.value.description);
 const descriptionEditing = computed(() => {
   return store.getDescriptionEditingStatus;
 });
-const description = ref(task.value.description);
+
+/* Watchers and Lifecycle hooks */
+watchEffect(() => {
+  if (titleInput.value) {
+    titleInput.value.focus();
+  }
+});
+
 onBeforeUnmount(() => {
   store.setEditingStatus(false);
 });
 
+/* Begin function declarations */
 function saveTitle() {
   if (taskTitle.value === task.value.title || !taskTitle.value) {
     titleEditing.value = false;
@@ -36,7 +41,7 @@ function saveTitle() {
         text: "No change to title detected",
         duration: 3000,
         newWindow: true,
-        className: "warning-msg",
+        className: "",
         close: true,
         gravity: "top", // `top` or `bottom`
         position: "right", // `left`, `center` or `right`
@@ -46,7 +51,7 @@ function saveTitle() {
     } else {
       Toastify({
         text: "Title cannot be empty!",
-        className: "error-msg",
+        className: "",
         duration: 3000,
         newWindow: true,
         close: true,
@@ -64,7 +69,7 @@ function saveTitle() {
     store.updateTaskTitle(task.value.id, taskTitle.value, generateTimeStamp());
     Toastify({
       text: "Title saved succcessfully!!",
-      className: "success-msg",
+      className: "",
       duration: 3000,
       newWindow: true,
       close: true,
@@ -85,7 +90,7 @@ function editTaskTitle() {
 
   Toastify({
     text: "Press enter to save!",
-    className: "info-msg",
+    className: "",
     duration: 3000,
     newWindow: true,
     close: true,
@@ -95,7 +100,7 @@ function editTaskTitle() {
     onClick: function () {}, // Callback after click
   }).showToast();
 }
-function handleDescSave(id, desc) {
+function handleDescriptionSave(id, desc) {
   store.updateTaskDescription(id, desc, generateTimeStamp());
   store.setEditingStatus(false);
 }
@@ -144,7 +149,7 @@ function handleDescSave(id, desc) {
           <button
             v-if="descriptionEditing"
             type="button"
-            @click="handleDescSave(task.id, description)"
+            @click="handleDescriptionSave(task.id, description)"
             class="btn"
           >
             Save Description
@@ -196,15 +201,7 @@ function handleDescSave(id, desc) {
               class="btn btn-sm"
               @click="store.setEditingStatus(true)"
             >
-              <svg
-                style="width: 1.7em"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
-                <path
-                  d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"
-                />
-              </svg>
+              <i class="bi bi-pencil"></i>
             </button>
           </div>
         </div>
@@ -222,17 +219,9 @@ function handleDescSave(id, desc) {
             <button
               type="button"
               class="btn btn-sm"
-              @click="handleDescSave(task.id, description)"
+              @click="handleDescriptionSave(task.id, description)"
             >
-              <svg
-                style="width: 1.7em"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 448 512"
-              >
-                <path
-                  d="M48 96V416c0 8.8 7.2 16 16 16H384c8.8 0 16-7.2 16-16V170.5c0-4.2-1.7-8.3-4.7-11.3l33.9-33.9c12 12 18.7 28.3 18.7 45.3V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96C0 60.7 28.7 32 64 32H309.5c17 0 33.3 6.7 45.3 18.7l74.5 74.5-33.9 33.9L320.8 84.7c-.3-.3-.5-.5-.8-.8V184c0 13.3-10.7 24-24 24H104c-13.3 0-24-10.7-24-24V80H64c-8.8 0-16 7.2-16 16zm80-16v80H272V80H128zm32 240a64 64 0 1 1 128 0 64 64 0 1 1 -128 0z"
-                />
-              </svg>
+              <i class="bi bi-pencil"></i>
             </button>
           </div>
         </div>
@@ -253,32 +242,6 @@ function handleDescSave(id, desc) {
 
 input {
   width: 100%;
-}
-
-.info-msg,
-.success-msg,
-.warning-msg,
-.error-msg {
-  margin: 10px 0 !important;
-  padding: 10px !important;
-  border-radius: 3px 3px 3px 3px !important;
-  background: none;
-}
-.info-msg {
-  color: #0d6efd;
-  border: 1px solid #0d6efd;
-}
-.success-msg {
-  color: hsla(160, 100%, 37%, 1);
-  border: 1px solid hsla(160, 100%, 37%, 1);
-}
-.warning-msg {
-  color: #ffa500;
-  border: 1px solid #ffa500;
-}
-.error-msg {
-  color: #ff6347;
-  border: 1px solid #ff6347;
 }
 
 textarea {
